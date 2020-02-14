8be5e7fe5544b85679f675c903c982121fa759c2
Amir Targholizadeh <amir.targholizadeh@gmail.com>
2020-02-14 14:29:33 -0800

    Strtification done. Branching starts from here.

--- main_CO2_emission.R
+++ main_CO2_emission.R
@@ -2,9 +2,18 @@
 
 library(data.table)
 library(ggplot2)
+library(splitstackshape)
+set.seed(1)
 
 getwd()
-CO2_emission <- fread("./data/MY2020 Fuel Consumption Ratings.csv")
+
+# Sample the data using Stratified method on variable "Make"
+#CO2_emission <- fread("./data/MY2020 Fuel Consumption Ratings.csv")
+#out <- stratified(CO2_emission, "Make", 0.23)
+#write.csv(out,'./data/sample_data.csv')
+
+#Read "sample_data.csv" and work with it from here on
+CO2_emission <- fread("./data/sample_data.csv")
 
 summary(CO2_emission)
 
